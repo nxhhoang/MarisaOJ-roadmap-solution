8aa6f15c5523d4fc6a76570a868eb2c3216ddbf9
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-01-02 22:17:22 +0700

    Create code.cpp

---
+++ Roadmap/level 1/Prefix sum/Difference array/Range update/code.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define pb push_back
+#define fi first
+#define se second
+#define endl '\n'
+#define umap unordered_map
+#define uset unordered_set
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+using namespace std;
+using ll = long long;
+
+const ll inf = 2e18;
+const int N = 10;
+const int MAXSIZE = 1e5;
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    int n, q, l;
+    cin >> n >> q;
+    vector<int> a(n + 1);
+    while (q--) cin >> l, a[l]++, cin >> l, a[l + 1]--;
+    FOR(i, 1, n + 1) cout << (a[i] += a[i - 1]) << " ";
+    cout << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
