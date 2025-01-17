f19045007d0083681f6c5649cc5d792b9b89f479
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-01-17 17:33:04 +0700

    Create code_v2_space_optimize.cpp

---
+++ Roadmap/Level 3/Bitmasks/Range XOR/code_v2_space_optimize.cpp
@@ -0,0 +1,44 @@
+// nxxhoang - the dreamer
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define endl '\n'
+#define ub upper_bound // find X < min(x)
+#define lb lower_bound // find X <= min(x)
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int NN = 1000 + 3;
+const int MAXSIZE = 2e3 + 5;
+const int MAXN = 1e6 + 3;
+const int MOD = 1e9 + 7;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+void solve() { 
+    int n, a;
+    ll sum = 0, M = 0, N = 0;
+    cin >> n;
+    FOR(i, 0, n) {
+        cin >> a;
+        a = __builtin_popcount(a) % 2;
+        (a) ? swap(M, N), M++ : N++;
+        sum += M;
+    }
+    cout << sum;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
