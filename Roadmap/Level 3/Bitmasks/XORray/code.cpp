f13a603c5efaafb2c5306f8e09b08a17a8188138
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-01-18 11:19:56 +0700

    Create code.cpp

---
+++ Roadmap/Level 3/Bitmasks/XORray/code.cpp
@@ -0,0 +1,40 @@
+// nxxhoang - the dreamer
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define endl '\n'
+#define ub upper_bound // find X < min(x)
+#define lb lower_bound // find X <= min(x)
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int NN = 1000 + 3;
+const int MAXSIZE = 2e3 + 5;
+const int MAXN = 1e6 + 3;
+const int MOD = 1e9 + 7;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+void solve(){
+    int n, a, result = 0, cur = 0;
+    cin >> n;
+    FOR (i, 1, n + 1) 
+        cin >> a, result ^= ((i % 2) ? (cur |= a) : (cur &= ~a));
+    
+    cout << result;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
