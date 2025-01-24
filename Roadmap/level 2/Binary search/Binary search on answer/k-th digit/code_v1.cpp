75b8d2d42f30aa568e7b871b99508ea99ffd758a
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-01-24 10:33:52 +0700

    Create code_v1.cpp

---
+++ Roadmap/level 2/Binary search/Binary search on answer/k-th digit/code_v1.cpp
@@ -0,0 +1,65 @@
+// nxxhoang - the dreamer
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define pb push_back
+#define fi first
+#define se second
+#define endl '\n'
+#define umap unordered_map
+#define uset unordered_set
+#define ub upper_bound // find X < min(x)
+#define lb lower_bound // find X <= min(x)
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int NN = 10;
+const int MAXSIZE = 2e5 + 5;
+const int MAXN = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+ull calc(ull x) {
+    if (x <= 9) return x;
+    ull ln = to_string(x).size() - 1;
+    ull ddd = 1ull * pow(10, ln);
+    return (x - ddd + 1) * (ln + 1) + calc(ddd - 1);  
+}
+
+void solve()
+{
+    ull k;
+    cin >> k;
+    k--;
+    ull l = 1; ull r = 1e18;
+    int kkk = 0;
+    while (l < r) {
+        ull mid = l + (r - l) / 2;
+        ull cnt = calc(mid - 1);
+        string s = to_string(mid);
+        ull ln = s.size() - 1;
+        if (cnt <= k && k <= cnt + ln) {
+            cout << s[k - cnt];
+            return;
+        } else if (k < cnt) r = mid;
+        else l = mid + 1;
+    }
+    cout << "-1" << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
