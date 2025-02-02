9a0aa893cccb8e4050eb6de9063883a90bf2d716
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-02-02 17:20:24 +0700

    Create code.cpp

---
+++ Roadmap/level 2/Math/Basic number theory/Prime number 2/code.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define pb push_back
+#define fi first
+#define se second
+#define endl '\n'
+#define umap unordered_map
+#define uset unordered_set
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+using namespace std;
+using ll = long long;
+
+const ll inf = 2e18;
+const int N = 10;
+const int MAXSIZE = 1e5;
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n <= 1) {
+        cout << "NO" <<endl;
+        return;
+    }
+    for(ll i = 2; i * i <= n; i++){
+        if (n % i == 0) {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
