b189e6135c47d342531739d174d4fe98619b63ad
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-02-05 10:23:26 +0700

    Update code_v3.cpp

--- Roadmap/Level 3/Dynamic Programming/Introduction to dynamic programming/code_v3.cpp
+++ Roadmap/Level 3/Dynamic Programming/Introduction to dynamic programming/code_v3.cpp
@@ -42,6 +42,10 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    solve();
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
