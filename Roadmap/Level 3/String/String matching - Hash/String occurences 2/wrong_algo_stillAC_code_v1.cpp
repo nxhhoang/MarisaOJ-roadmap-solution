993013fa02643aa9767bc29ba558336eca6e75dd
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-02-07 21:04:42 +0700

    Create wrong_algo_stillAC_code_v1.cpp

---
+++ Roadmap/Level 3/String/String matching - Hash/String occurences 2/wrong_algo_stillAC_code_v1.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define pb push_back
+#define fi first
+#define se second
+#define endl '\n'
+#define umap unordered_map
+#define uset unordered_set
+#define ub upper_bound
+#define lb lower_bound
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int N = 10;
+const int MAXSIZE = 1e5;
+const int MAXN = 1e6;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    string s, t;
+    cin >> s >> t;
+
+    int pos = -1;
+
+    int index = 0;
+    int count = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (index < t.size() && s[i] == t[index]) {
+            index++;
+            if (s[i + 1] == t[0]) pos = i;
+        } else {
+            index = 0;
+            if (pos != -1) i = pos, pos = -1;
+        }
+        
+        if (index == t.size()) {
+            count++, index = 0;
+            if (pos != -1) i = pos, pos = -1;
+        }
+    }
+
+    cout << count << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
