f7976d40da6ca8f27e4e057c1bced0d871be1f71
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-02-16 12:13:32 +0700

    Create code_v2.cpp

---
+++ Roadmap/level 2/Math/Basic number theory/Divisors counting/code_v2.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define endl '\n'
+
+using namespace std;
+using ll = long long;
+
+const ll inf = 2e18;
+const int N = 10;
+const int MAXSIZE = 1e5;
+const int MAXN = 1e6 + 1;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    int q;
+    cin >> q;
+    int cnt[MAXN] = {0};
+
+    for (int i = 1; i < MAXN; ++i) {
+        for (int j = i; j < MAXN; j += i) ++cnt[j];
+        cnt[i] += cnt[i - 1];
+    }
+
+    while (q--){
+        int l, r;
+        cin >> l >> r;
+        int ans = cnt[r] - cnt[l - 1];
+        cout << ans << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
