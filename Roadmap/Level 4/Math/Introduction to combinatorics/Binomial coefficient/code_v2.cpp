3c533961ae48eb73c5cbb77d7461ca1191a7e886
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-02-17 13:13:34 +0700

    Create code_v2.cpp

---
+++ Roadmap/Level 4/Math/Introduction to combinatorics/Binomial coefficient/code_v2.cpp
@@ -0,0 +1,66 @@
+// nxxhoang - the dreamer
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define endl '\n'
+#define ub upper_bound // find target < min(value)
+#define lb lower_bound // find target <= min(value)
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int NN = 10;
+const int MAXSIZE = 1e5 + 5;
+const int MAXN = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+ull overfl(ll a, ll b) {
+    ll result = 1;
+    a = a % MOD;
+    
+    while (b > 0) {
+        if (b & 1) result = result * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+ 
+    return result;
+}
+
+ll fac[MAXN];
+ll dao[MAXN];
+void solve() {
+    int q;
+    cin >> q;
+    fac[0] = dao[0] = 1;
+    for (int i = 1; i < MAXN; i++) fac[i] = i * fac[i - 1] % MOD;
+    dao[MAXN - 1] = overfl(fac[MAXN - 1], MOD - 2);
+    for (int i = MAXN - 1; i >= 1; i--) dao[i - 1] = dao[i] * i % MOD;
+    
+	while(q--){
+        int n, k;
+        cin >> n >> k;
+        int val = (fac[n] * dao[k] % MOD) * dao[n - k] % MOD;
+        cout << val << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
