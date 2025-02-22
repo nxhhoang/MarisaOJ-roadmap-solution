02b9174b99999529f5d1d78dcb8491bb66b5556f
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-02-23 06:58:53 +0700

    Create code.cpp

---
+++ Roadmap/level 2/Math/Basic number theory/GCD and LCM/code.cpp
@@ -0,0 +1,53 @@
+// nxxhoang - the dreamer
+#include<bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define pb push_back
+#define fi first
+#define se second
+#define endl '\n'
+#define umap unordered_map
+#define uset unordered_set
+#define ub upper_bound
+#define lb lower_bound
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int NN = 10;
+const int MAXSIZE = 1e5 + 5;
+const int MAXN = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int dx[] = {1, -1, 0, 0}; 
+const int dy[] = {0, 0, 1, -1};
+
+void solve() {
+    ll u, v;
+    cin >> u >> v;
+
+    int sum = 0;
+    ll tich = u * v;
+    for (int i = u; i <= (ll)sqrt(tich) ; i += u) {
+        int doi_u = tich / i;
+        if (doi_u % u || __gcd(doi_u, i) != u) continue;
+        if (doi_u != u) sum += 2;
+        else sum++;
+    }
+
+    cout << sum << endl;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
