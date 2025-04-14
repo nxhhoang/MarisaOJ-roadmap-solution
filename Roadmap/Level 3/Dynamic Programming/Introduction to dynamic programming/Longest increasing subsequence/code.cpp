0321534b2f39ce4b91bea4cc9499057b7205953f
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-04-15 06:38:24 +0700

    Create code.cpp

---
+++ Roadmap/Level 3/Dynamic Programming/Introduction to dynamic programming/Longest increasing subsequence/code.cpp
@@ -0,0 +1,53 @@
+// nxxhoang - the dreamer
+#include<bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define pb push_back
+#define fi first
+#define se second
+#define endl '\n'
+#define umap unordered_map
+#define uset unordered_set
+#define ub upper_bound  // find X < min(x)
+#define lb lower_bound  // find X <= min(x)
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int NN = 10;
+const int MAXSIZE = 1e5 + 5;
+const int MAXN = 2e6 + 1;
+const int MOD = 1e9 + 7;
+const int dx[] = {1, -1, 0, 0}; 
+const int dy[] = {0, 0, 1, -1};
+
+void solve() {    
+    int n;
+    cin >> n;
+	vector<int> A(n);
+	for (int i = 0; i <n ; i++) cin >> A[i];
+	vector<int> dp(n, 1);
+	int maxV = 0;
+	for (int i = 0; i < n; i++){
+		for (int j = i - 1; j >= 0; j--){
+			if (A[j] < A[i])
+			dp[i] = max(dp[i], dp[j] + 1);
+		}
+		maxV = max(maxV, dp[i]);
+	}
+	cout << maxV;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
