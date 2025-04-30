3a1dfc3c9accdd2d5eb92157138d64324716a55b
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-04-30 07:16:09 +0700

    Create code_v2.cpp

---
+++ Roadmap/Level 3/Bitmasks/Bitwise operations 2/code_v2.cpp
@@ -0,0 +1,47 @@
+// nxxhoang - the dreamer
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define endl '\n'
+#define ub upper_bound // find X < min(x)
+#define lb lower_bound // find X <= min(x)
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int NN = 10;
+const int MAXSIZE = 2e3 + 5;
+const int MAXN = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+void solve() { 
+    int q;
+    cin >> q;
+    while (q--) {
+        int a, b;
+        cin >> a >> b;
+        int t = 31 << 10;
+        int sum = (a & t) | (b & t);
+        t >>= 5;
+        sum += (a & t) & (b & t);
+        t >>= 5;
+        sum += (a & t) ^ (b & t);
+        cout << sum << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
