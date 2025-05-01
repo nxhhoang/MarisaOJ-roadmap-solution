58663344dff179ed387cd32a2801238c6acf5d16
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-05-02 00:20:10 +0700

    Create code_v2.cpp

---
+++ Roadmap/Level 3/Bitmasks/XOR pair/code_v2.cpp
@@ -0,0 +1,45 @@
+// nxxhoang - the dreamer
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define endl '\n'
+#define ub upper_bound // find X < min(x)
+#define lb lower_bound // find X <= min(x)
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int NN = 10;
+const int MAXSIZE = 2e3 + 5;
+const int MAXN = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+void solve() { 
+    int n, a;
+    cin >> n;
+    unordered_map<int, int> mp;
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> a;
+        int b = a ^ i;
+        if (mp.find(b) != mp.end()) cnt += mp[b];
+        mp[b]++;
+    }
+    cout << cnt << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
