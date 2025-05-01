73693c241062ef40f467c86a621309a1b158c39b
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-05-01 09:40:02 +0700

    Create code.cpp

---
+++ Roadmap/Level 3/Bitmasks/Bitwise operations 3/code.cpp
@@ -0,0 +1,45 @@
+// nxxhoang - the dreamer
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define endl '\n'
+#define ub upper_bound // find X < min(x)
+#define lb lower_bound // find X <= min(x)
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int NN = 10;
+const int MAXSIZE = 2e3 + 5;
+const int MAXN = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+void solve() { 
+    int q;
+    cin >> q;
+    int a = 0;
+    while (q--) {
+        int x;
+        cin >> x;
+        if (x == 1) cin >> x, a ^= x;
+        else if (x == 2) a &= ~(1 << (31 - __builtin_clz(a)));
+        else cout << __builtin_popcount(a) << endl;
+        
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
