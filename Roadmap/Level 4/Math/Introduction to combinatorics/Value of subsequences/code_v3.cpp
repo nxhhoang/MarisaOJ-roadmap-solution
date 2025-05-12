21daf4dbf0c3a3b0afe7659822886341d346a25f
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-05-12 08:45:43 +0700

    Create code_v3.cpp

---
+++ Roadmap/Level 4/Math/Introduction to combinatorics/Value of subsequences/code_v3.cpp
@@ -0,0 +1,64 @@
+// nxhhoang - the dreamer
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define endl '\n'
+#define ub upper_bound // find target < min(value)
+#define lb lower_bound // find target <= min(value)
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int NN = 10;
+const int MAXSIZE = 1e5 + 5;
+const int MAXN = 1e6 + 3;
+const int MOD = 1e9 + 7;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+ull overfl(ll a, ll b) {
+    ll result = 1;
+    a %= MOD;
+    while (b > 0) {
+        if (b & 1) result = result * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+ 
+    return result;
+}
+void solve() { 
+    vector<int> fac(MAXSIZE);
+    vector<int> un_fac(MAXSIZE); // x1 + x2 + x3 = 8 
+    int n, k, p; cin >> n >> k;
+    fac[0] = fac[1] = 1;
+    for (int i = 1; i < MAXSIZE; i++) fac[i] = 1ll * fac[i - 1] * i % MOD;
+    un_fac[MAXSIZE - 1] = overfl(fac[MAXSIZE - 1], MOD - 2);        
+    for (int i = MAXSIZE - 1; i >= 1; i--) un_fac[i - 1] = 1ll * un_fac[i] * i % MOD; 
+    int arr[MAXSIZE];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+    int dem = 0;
+    ll res = 0;
+    for (int i = k; i <= n; i++) 
+        res += ((1ll * fac[i - 1] * un_fac[k - 1] % MOD) * un_fac[i - k] % MOD) * arr[i - 1] % MOD;
+    cout << res % MOD;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    // cin >> t; LLLLLLLLLLLLLLLLLLLLL
+    while(t--){
+        solve();
+    }
+    return 0;
+}
