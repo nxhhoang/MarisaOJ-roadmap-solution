1946b87435222a0cf30685175f7c43f39bf7e431
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-05-16 05:45:55 +0700

    Create code.cpp

---
+++ Competition/MarisaOJ Contest - Round #0.25/Hotspot/code.cpp
@@ -0,0 +1,51 @@
+// nxhhoang - the dreamer
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define endl '\n'
+#define ub upper_bound // find target < min(value)
+#define lb lower_bound // find target <= min(value)
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int NN = 10;
+const int MAXSIZE = 1e5 + 5;
+const int MAXN = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    int minL = n;
+    int maxR = 1;
+
+    while(q--) {
+        int l, r;
+        cin >> l >> r;
+        if (l > r) swap(l,r);
+        minL = min(l, minL);
+        maxR = max(maxR, r);
+        cout << 1ll * (minL - 1) * (n - maxR) << endl;        
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
