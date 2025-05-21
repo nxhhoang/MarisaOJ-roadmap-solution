8ced850ef736764b807518836af83a87d0e13159
nxhhoang <150522733+nxhhoang@users.noreply.github.com>
2025-05-22 06:00:29 +0700

    Create code_v6.cpp

---
+++ Roadmap/Level 3/Bitmasks/Minimum Xor Pair Query/code_v6.cpp
@@ -0,0 +1,157 @@
+// nxhhoang - the dreamer
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i < (int)b; i++)
+#define endl '\n'
+#define ub upper_bound // find target < min(value)
+#define lb lower_bound // find target <= min(value)
+
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const ll inf = 2e18;
+const int NN = 10;
+const int MAXSIZE = 1e5 + 5;
+const int MAXN = 1e6 + 1;
+const int MOD = 1e9 + 7;
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+using sint = signed int;
+
+struct Node{
+    sint data; // Min
+    // sint val; // Value for xor
+    sint cnt;
+    Node* left;
+    Node* right;
+    Node() : data(INT_MAX), cnt(0), left(nullptr), right(nullptr) {}
+    // Node() : data(INT_MAX), cnt(0), val(INT_MAX), left(nullptr), right(nullptr) {}
+};
+
+void addNode(Node* root, int i, int val) {
+    if (i == 30) {
+        if (root->cnt >= 2) root->data = 0;
+        else root->data = val;
+        return;
+    }
+
+    if (((val >> (29 - i)) & 1) == 0) {
+        if (root->left == nullptr) root->left = new Node();
+        root->left->cnt++, root->left->data = val;
+        addNode(root->left, i + 1, val);
+    } else { 
+        if (root->right == nullptr) root->right = new Node();
+        root->right->cnt++, root->right->data = val;
+        addNode(root->right, i + 1, val);
+    }
+
+    if (root->left && root->right == nullptr) {
+        if (root->left->cnt == 1) root->data = root->left->data;
+        else root->data = root->left->data;
+        return;
+    }
+
+    if (root->right && root->left == nullptr) {
+        if (root->right->cnt == 1) root->data = root->right->data;
+        else root->data = root->right->data;
+        return;
+    }
+
+    if (root->left->cnt >= 2 && root->right->cnt >= 2) root->data = min(root->left->data, root->right->data);
+    else if (root->left->cnt >= 2) root->data = root->left->data;
+    else if (root->right->cnt >= 2) root->data = root->right->data;
+    else if (root->left->cnt == 1 && root->right->cnt == 1) root->data = root->left->data ^ root->right->data;
+    else if (root->left->cnt == 1) {
+        root->data = root->left->data;
+    } 
+    else if (root->right->cnt == 1) {
+        root->data = root->right->data;
+    }
+}
+
+void removeNode(Node* root, int i, int val) {
+    if (i == 30) {
+        if (root->cnt == 0) root->data = INT_MAX;
+        else if (root->cnt == 1) root->data = INT_MAX;
+        return;    
+    }
+
+    if (((val >> (29 - i)) & 1) == 0) {
+        root->left->cnt--;
+        removeNode(root->left, i + 1, val);
+    } else {
+        root->right->cnt--;
+        removeNode(root->right, i + 1, val);
+    } 
+
+    if (root->left && root->right == nullptr) {
+        if (root->left->cnt == 1) root->data = root->left->data;
+        else root->data = root->left->data;
+        return;
+    }
+
+    if (root->right && root->left == nullptr) {
+        if (root->right->cnt == 1) root->data = root->right->data;
+        else root->data = root->right->data;
+        return;
+    }
+
+    if (root->left->cnt >= 2 && root->right->cnt >= 2) root->data = min(root->left->data, root->right->data);
+    else if (root->left->cnt >= 2) root->data = root->left->data;
+    else if (root->right->cnt >= 2) root->data = root->right->data;
+    else if (root->left->cnt == 1 && root->right->cnt == 1) root->data = root->left->data ^ root->right->data;
+    else if (root->left->cnt == 1) {
+        root->data = root->left->data;
+    } 
+    else if (root->right->cnt == 1) {
+        root->data = root->right->data;
+    }
+}
+
+void deleteNode(Node* root) {
+    if (root == nullptr) return;
+    deleteNode(root->left);
+    deleteNode(root->right);
+    delete root;
+    root = nullptr;
+}
+
+void solve() {
+    int q;
+    cin >> q;
+    int d, x;
+    string s;
+    
+    Node* root = new Node();
+    while (q--) {
+        cin >> d;
+        if (d == 1) {
+            cin >> x;
+            addNode(root, 0, x);
+        } else if (d == 2) {
+            cin >> x;
+            removeNode(root, 0, x);
+        } else {
+            cout << root->data << endl;
+        }
+    }
+    deleteNode(root);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
